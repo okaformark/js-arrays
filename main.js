bd805d064060bbcb40ef48f89b78366f5ac9260e
Mark Okafor <okaformark@gmail.com>
2019-03-05 19:30:22 -0600

    added html js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('js arrays');
+const dinosaurs = [
+    {
+        dinoType: 't-rex'
+    },
+    {
+        dinoType: 'stegosaurus'
+    },
+    {
+        dinoType: 'velociraptor'
+    }
+];
+
+//loop over the array
+//build up a domstring
+//print domstring to the dom
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const buildDinosaurs = () => {
+    let domString = "";
+    for(let i = 0; i < dinosaurs.length; i++){
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+    }
+    printToDom('dino-barn', domString);
+};
+
+const init = () =>{
+    buildDinosaurs();
+};
+init();
